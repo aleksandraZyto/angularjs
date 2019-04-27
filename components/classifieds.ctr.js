7ede5fbfdbdac0370f66eaaff024d027b83e304a
aleksandraZyto <rydwanapplication@gmail.com>
2019-04-27 17:29:57 +0200

    Adding classifieds

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,14 +4,38 @@
     
     angular
     .module("ngClassifieds")
-    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav){
+    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
         
         classifiedsFactory.getClassifieds().then(function(classifieds){
             $scope.classifieds = classifieds.data;    
         });
         
+        var contact = {
+            name:"Ola Zyto",
+            phone:"667939808",
+            email:"aleksadra.zyto@gmail.com"
+        };
+        
         $scope.openSidebar = function(){
             $mdSidenav('left').open();
+        }
+        
+        $scope.closeSidebar = function(){
+            $mdSidenav('left').close();
+        }
+        
+        $scope.saveClassified = function(classified){
+            if(classified){
+                $scope.classifieds.push(classified);
+                $scope.classified.contact = contact;
+                $scope.classified = {};
+                $scope.closeSidebar();
+                $mdToast.show(
+                    $mdToast.simple()
+                    .content("New Classified saved")
+                    .hideDelay(3000)
+                    .position("top, right"));
+            }
         }
     });
 })();
